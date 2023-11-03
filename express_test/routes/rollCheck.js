e5fca61a2f825ec463f5c10c59c88569e7a96a30
vtwarrior25 <vtwarrior25@gmail.com>
2023-11-03 18:05:36 -0400

    Init

---
+++ express_test/routes/rollCheck.js
@@ -0,0 +1,68 @@
+var express = require('express');
+var router = express.Router();
+
+function rollDice (die, num) { 
+  console.log(`die = ${die}, num = ${num}`);
+  dicerolls = [];
+  for (let i = 0; i < num; i++) {
+    dicerolls.push(Math.ceil(Math.random() * Number(die)));
+  }
+  return dicerolls;
+}
+
+function rollCheck(name, rolltype, die, number, mod){
+  check = {
+    rolls: [],
+    rollstring: "",
+    rolltotal: "",
+    basestring: "",
+    name: "",
+    rolltype: "",
+  }
+
+  check.name = name;
+  check.rolltype = rolltype;
+
+  dicerolls = rollDice(Number(die), Number(number));
+  rolltotal = 0;
+  dicerolls.forEach( num => {
+    rolltotal += num;
+    check.rolls.push(num);
+  })
+  rolltotal = rolltotal + Number(mod);
+  rollstring = '';
+  basestring = '';
+  i = 0;
+  for (roll of dicerolls){
+    if (Number(roll) < 0 || i == 0) {
+      rollstring = `${rollstring} ${roll}` 
+    } else {
+      rollstring = `${rollstring}+${roll}` 
+    }
+    i++;
+  }
+  if (Number(mod) < 0) {
+    rollstring = `${rollstring}${mod}`
+    basestring = `${number}d${die}${mod}`
+  } else if (Number(mod) == 0) {
+    rollstring = `${rollstring}`
+    basestring = `${number}d${die}`
+  } else {
+    rollstring = `${rollstring}+${mod}`
+    basestring = `${number}d${die}+${mod}`
+  }
+
+  check.rollstring = rollstring.trim();
+  check.rolltotal = rolltotal;
+  check.basestring = basestring;
+  return check;
+}
+
+router.get('/', function(req, res, next) {
+  let q = req.query;
+  check = rollCheck(q.name, q.rolltype, Number(q.die), Number(q.num), Number(q.mod));
+  console.log(`rolltotal = ${rolltotal}`);
+  res.send(check);
+});
+
+module.exports = router;
---
+++ express_test/routes/testAPI.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+var testobject = {
+  item: {
+    name: "Scale Mail",
+    value: 50,
+    description: "This armor consists of a coat and leggings (and perhaps a separate skirt) of leather covered with overlapping pieces of metal, much like the scales of a fish. The suit includes gauntlets.",
+    weight: 45,
+    rarity: {
+      rarityname: "",
+      raritydescription: "",
+    }
+  },
+  armorclass: 14,
+  strengthreq: 0,
+  stealthdisadvantage: true,
+  word: 'booty',
+}
+
+function beans() {
+  return "Bungus Beans";
+}
+
+
+router.get('/', function(req, res, next) {
+  let queries = req.query;
+  testobject.word = "booty"; 
+  if (queries.name == "bungus") {
+    testobject.word = beans();
+  } 
+    res.send(testobject);
+});
+
+module.exports = router;
