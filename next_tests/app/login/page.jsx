817a982008c63c66b9491d011e6ec5920e6626d1
Stirling S <stirlingjohn612@gmail.com>
2024-02-23 11:33:04 -0500

    updated css for login page
    Login section is now encapsulated in one div and is centered. Individual items in the form are also centered.

--- next_tests/app/login/page.jsx
+++ next_tests/app/login/page.jsx
@@ -2,18 +2,22 @@
 
 import { authenticate } from '@/app/lib/actions'
 import { useFormState, useFormStatus } from 'react-dom'
+import './login.css';
 
 export default function Page() {
   const [errorMessage, dispatch] = useFormState(authenticate, undefined)
   const authenticatemode = authenticate.bind(null);
 
   return (
-    <form action={authenticatemode}>
-      <input type="text" name="username" placeholder="Username" required />
-      <input type="password" name="password" placeholder="Password" required />
-      <div>{errorMessage && <p>{errorMessage}</p>}</div>
-      <LoginButton />
-    </form>
+    <div className='loginsection'>
+      <h3>Welcome to Questweaver login page</h3>
+      <form action={authenticatemode}>
+        <input type="text" name="username" placeholder="Username" required />
+        <input type="password" name="password" placeholder="Password" required />
+        <div>{errorMessage && <p>{errorMessage}</p>}</div>
+        <LoginButton />
+      </form>
+    </div>
   )
 }
 
