f743124c5907e1c1b44f4500f2f2b071611e5216
Stirling S <stirlingjohn612@gmail.com>
2024-03-29 09:05:56 -0400

    Updated monster group adder
    The function currently adds new encounters and adds monster groups to encounters. Next priority is inserting ability and attack data

--- next_tests/app/lib/monster.js
+++ next_tests/app/lib/monster.js
@@ -42,7 +42,7 @@ const getencounternamequery = new PQ({
 const addnewmonstergroupquery = new PQ({
   text: `
   INSERT INTO monstergroup (monstergroupid, encounterid, creaturesize, monstertypeid, alignment, groupname, description, hitdie, hitdienum, challengerating, xp, armorclass, speed, initiative, skills, features, notes)
-  VALUES (DEFAULT, $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16)`
+  VALUES (DEFAULT, (SELECT encounterid FROM encounter WHERE name = $1), $2, (SELECT monstertypeid FROM monstertype WHERE name = $3), $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16)`
 });
 
 const addnewencounterquery = new PQ({
@@ -60,14 +60,24 @@ export async function addgroupfromform(formdata, encountername) {
     for (ename of encounternames) {
       if (ename == encountername) {
         // add monster group to monstergroup table with encounterid attatched
+        db.one(addnewmonstergroupquery, [encountername, formdata.basicinfo.size, formdata.basicinfo.type, formdata.basicinfo.alignment, formdata.basicinfo.name, formdata.basicinfo.description, formdata.basicinfo.hitdicetype, formdata.basicinfo.hitdicenum, formdata.basicinfo.challengerating, formdata.basicinfo.xptotal, formdata.basicinfo.ac, formdata.basicinfo.speed, formdata.abilities.init, formdata.skills, formdata.ability, formdata.notes])
+        .catch((error) => {
+          console.log("Error adding new monster group to encounter: " + error);
+          return;
+        });
         break;
       }
     }
     if (ename != encountername) {
       // add new encounter to encounter table, and add monster group
       db.one(addnewencounterquery, encountername)
-      .then({
+      .then((result) => {
         // add monster group to monstergroup table with encounterid attatched
+        db.one(addnewmonstergroupquery, [encountername, formdata.basicinfo.size, formdata.basicinfo.type, formdata.basicinfo.alignment, formdata.basicinfo.name, formdata.basicinfo.description, formdata.basicinfo.hitdicetype, formdata.basicinfo.hitdicenum, formdata.basicinfo.challengerating, formdata.basicinfo.xptotal, formdata.basicinfo.ac, formdata.basicinfo.speed, formdata.abilities.init, formdata.skills, formdata.ability, formdata.notes])
+        .catch((error) => {
+          console.log("Error adding new monster group to encounter: " + error);
+          return;
+        });
       })
       .catch((error) => {
         console.log("Error adding new encounter: " + error);
